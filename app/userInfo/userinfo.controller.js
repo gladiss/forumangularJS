edc622bbed0ee5e9be1bad5160f52d21fbef404f
gladiss <gladiss@163.com>
2017-08-03 15:47:06 +0800

    refine userinfo page

--- app/userInfo/userinfo.controller.js
+++ app/userInfo/userinfo.controller.js
@@ -2,26 +2,29 @@
 angular.module("cambricon-forum").controller("userInfoController",
     ["$scope", "$stateParams", "$http", "toastr", "$state", "localStorageService", function ($scope, $stateParams, $http, toastr, $state, localStorageService) {
 
-        $("#userInfo").height(window.innerHeight);
+        $("#userInfo").height(window.innerHeight*2);
 
         $scope.name = $stateParams.username;
         $scope.loginname = localStorageService.get("username");
 
-        $scope.selectUserInfo = function () {
-            $scope.user = {};
+        $scope.user = {};
+        $scope.userinfo = function(){
+            $("#myinfo").show();
+            $("#myreply").hide();
+            $("#myarticle").hide();
+            $("#myfile").hide();
+            $("#file").removeClass("fontcolor");
+            $("#reply").removeClass("fontcolor");
+            $("#subject").removeClass("fontcolor");
+            $("#person").addClass("fontcolor");
             $http.post(SERVER_URL + "/users/getUserInfo", {"username": $stateParams["username"]})
                 .then(function (response) {
                     $scope.user = response.data;
-                    console.log($scope.user.user_img);
-
                 })
                 .catch(function (error) {
                     toastr.error(error.data.err);
                 });
-
-
         };
-
         $scope.visible = true;
         $scope.toggle = function () {
             $scope.visible = !$scope.visible;
@@ -46,13 +49,82 @@ angular.module("cambricon-forum").controller("userInfoController",
             });
         };
 
-        $scope.cancel = function ($state, $stateParams) {
+        $scope.cancel = function () {
             $scope.toggle();
             $scope.selectUserInfo();
         };
 
-        $scope.selectUserInfo();
 
-    }]);
+        $scope.toggleArticle = function () {
+            $("#myarticle").show();
+            $("#reply").removeClass("fontcolor");
+            $("#person").removeClass("fontcolor");
+            $("#file").removeClass("fontcolor");
+            $("#subject").addClass("fontcolor");
+            $("#myreply").hide();
+            $("#myinfo").hide();
+            $("#myfile").hide();
+            $scope.mytopiclist();
+
+        };
+        $scope.toggleReply =function(){
+            $("#person").removeClass("fontcolor");
+            $("#subject").removeClass("fontcolor");
+            $("#file").removeClass("fontcolor");
+            $("#reply").addClass("fontcolor");
+            $("#myreply").show();
+            $("#myarticle").hide();
+            $("#myinfo").hide();
+            $("#myfile").hide();
+            $http.post(SERVER_URL + "/comment/getCommentByUser", {"username": $stateParams["username"]})
+                .then(function (response) {
+                    $scope.replys = response.data;
+                })
+                .catch(function (error) {
+                    if (error.data) {
+                        toastr.error(error.data);
+                    } else {
+                        toastr.error("连接服务器失败!");
+                    }
+                });
+        };
+        $scope.toggleFile =function(){
+            $("#person").removeClass("fontcolor");
+            $("#subject").removeClass("fontcolor");
+            $("#reply").removeClass("fontcolor");
+            $("#file").addClass("fontcolor")
+            $("#myreply").hide();
+            $("#myarticle").hide();
+            $("#myinfo").hide();
+            $("#myfile").show();
+            $http.post(SERVER_URL + "/file/getFileByUser", {"username": $stateParams["username"]})
+                .then(function (response) {
+                    $scope.files = response.data;
+                })
+                .catch(function (error) {
+                    if (error.data) {
+                        toastr.error(error.data);
+                    } else {
+                        toastr.error("连接服务器失败!");
+                    }
+                });
+        };
+
+        $scope.mytopiclist = function () {
+            $http.post(SERVER_URL + "/forum/getForumByUser", {"username": $stateParams["username"]})
+                .then(function (response) {
+                    $scope.articles = response.data;
+                })
+                .catch(function (error) {
+                    if (error.data) {
+                        toastr.error(error.data.err);
+                    } else {
+                        toastr.error("连接服务器失败!");
+                    }
+                });
 
 
+        };
+        $scope.mytopiclist();
+    }
+    ]);
